5cef97fe979014d54406f53adeeb64dbbc408d2a
ssande7 <1731652+ssande7@users.noreply.github.com>
2024-06-12 15:53:56 +1000

    Replace Myst notes with Solara widgets

--- helpers.py
+++ helpers.py
@@ -358,13 +358,236 @@ import solara.lab
 
 @solara.component
 def Step1():
-    with solara.lab.Tabs():
-        with solara.lab.Tab('Instructions: Loading a molecule'):
-            solara.Markdown(
+    with solara.Details(summary=solara.Markdown('### Step 1: Loading in a molecule'), expand=False):
+        with solara.lab.Tabs():
+            with solara.lab.Tab('Instructions'):
+                solara.Markdown(
 '''
-1. To run the calculation, we first need to tell the quantum chemistry software which molecule to look at. We can do this with a SMILES code, which we will get from the table in the next tab, or from another website.
-2. For some common molecules, you can copy the this from the table in the "SMILES Codes" tab of this panel and paste it in the box below.
-3. Click "Load Molecule" to load in the SMILES code and check that it's correct
+We first need to tell the quantum chemistry software which molecule to look at, which we can do with a SMILES code.
+"SMILES" stands for **simplified molecular-input line-entry system** and is a useful standard format for describing
+molecules to simple computer programs - like this notebook!
+
+The "InputSMILES" box below has the SMILES code for chloroform filled in by default -- when you click "Load Molecule",
+you can check that the notebook displays a 2D structure of the correct molecule.
+
+After you have completed the calculations for a chloroform molecule, you can look at the table in the next tab
+("Some SMILES codes") and input SMILES codes for other common molecules, and then click "Load Molecule" to start
+calculations for any of them.
+
+If you'd like to look at other molecules not in that list, check out the "Building Molecules in Molview" tab
+to see how to build molecules in Molview and obtain their SMILES codes!
 ''')
-        with solara.Tab('Information: SMILES Codes'):
-            solara.Markdown('TODO - testing')
+            with solara.lab.Tab('Some SMILES Codes'):
+                with solara.Columns([1,1,2]):
+                    with solara.Column():
+                        solara.Markdown('#### Molecule')
+                        solara.Text('Chloroform')        
+                        solara.Text('Methane')           
+                        solara.Text('Dichloromethane')   
+                        solara.Text('Acetonitrile')      
+                        solara.Text('Dimethylsulfoxide') 
+                        solara.Text('Dimethylformamide') 
+                        solara.Text('Benzene')           
+                        solara.Text('Pyridine')          
+                        solara.Text('Phenol')            
+                        solara.Text('Aniline')           
+                    with solara.Column():
+                        solara.Markdown('#### SMILES')
+                        solara.Text('ClC(Cl)Cl')
+                        solara.Text('C')
+                        solara.Text('ClCCl')
+                        solara.Text('CC#N')
+                        solara.Text('CS(=O)C')
+                        solara.Text('CN(C)C=O')
+                        solara.Text('C1=CC=CC=C1')
+                        solara.Text('C1=CC=NC=C1')
+                        solara.Text('C1=CC=C(C=C1)O')
+                        solara.Text('C1=CC=C(C=C1)N')
+                    solara.Column()
+            with solara.lab.Tab('SMILES codes from Moleview'):
+                solara.Markdown(
+'''
+If you'd like to look at a molecule that's not on the list, you can get a SMILES code from
+[Molview](https://molview.org/):
+
+<ol>
+    <li>
+    Once Molview has loaded, click on the "Clear all" button (with a _rubbish bin_ logo) to clear the screen and get ready for your own molecule!
+    <ul>
+        <li>The <i>white</i> area on the left will be cleared out. Don't worry about the <i>black</i> area on the right with a 3D molecular model - we will generate a new 3D model soon.</li>
+    </ul>
+    </li>
+
+    <li>
+    Now try building a chloroform molecule in the white area (if you get stuck, see instructions in the "(Molview) Building a chloroform molecule" tab!)
+    <ul>
+        <li><i><b>Hint:</b> For other molecules, if you're not sure how they look, try searching for their Lewis structure online, or looking them up by name with Molview's search bar.</i></li>
+    </ul>
+    </li>
+
+    <li>
+    Once you have built the molecule, here's how you get the SMILES code:
+    <ol>
+        <li>Look for the "Tools" dropdown menu, and then click on the "Information Card" in the middle of the menu.</li>
+        <li>Look for an entry called "Canonical SMILES". You will see a series of upper-case letters, brackets, and equal-signs. That's the SMILES code!</li>
+        <li>Copy it into the "InputSMILES" box in this notebook below, then click the "Load Molecule" button and check that the same molecule is loaded into the notebook.</li>
+        <li>Now we can do some more accurate calculations on that molecule than what is possible with Molview.</li>
+    </ol>
+    </li>
+</ol>
+''')
+            with solara.lab.Tab('Molview: Building a chloroform molecule'):
+                solara.Markdown(
+'''
+Here's how you could build a chloroform molecule in Molview:
+
+<ol>
+    <li>Use "Clear all" to start again!</li>
+
+    <li>
+    The <i>right sidebar</i> has a list of elements to choose from.
+    <ul>
+        <li>Click on "C" (carbon) at the top right. Now click anywhere in the white area.</li>
+        <li>This creates a new carbon atom. When you hover your mouse over the carbon atom, it will be highlighted in green.</li>
+    </ul>
+    </li>
+
+    <li>
+    The <i>left sidebar</i> has a list of common chemical bonds. 
+    <ul>
+        <li>Click on the line (single bond) at the top left.</li>
+        <li>Now hover on the carbon atom and click <i>three times</i>. This creates three single bonds joined to the carbon atom.</li>
+    </ul>
+    </li>
+    
+    <li>
+    Lastly, click on the "Cl" (chlorine) atom on the right, and then click on the ends of the bonds to change each atom on the end to a Cl atom.
+    <ul>
+        <li>Notice we don't need to add hydrogen atoms! Molview assumes that any un-bonded outer shell electrons will find a hydrogen, and adds them in automatically.</li>
+    </ul>
+    </li>
+</ol>
+''')
+
+
+@solara.component
+def Step2():
+    with solara.Details(summary=solara.Markdown('### Step 2: Running quantum geometry calculations'), expand=False):
+        with solara.lab.Tabs():
+            with solara.lab.Tab('Instructions: Click the buttons!'):
+                solara.Markdown(
+'''
+1. Underneath this box, click the "Run Quantum Optimization" button. This will take a little time, and then print out when it's done.
+2. Once the calculation is done, click the "Show Minimized State"  button to see the molecule in 3D!
+''')
+            with solara.lab.Tab('Information: Energy Minimization and Molecular Vibration Searches'):
+                solara.Markdown(
+'''
+This notebook runs through two calculations. The first is an _energy minimization_ (also called geometry optimization),
+where the underlying program adjusts the coordinates of individual atoms, and then calculates the total energy the
+molecule would have in that state. These calculations are repeated until a state is found at which the molecule has
+minimum energy - like a ball rolling down to the bottom of a bowl, this will be the most stable state for the molecule.
+
+The next calculation is a _vibrational analysis_. The program adjusts the atoms' coordinates again - but instead of
+finding the minimum energy, it records how the energy changes with the atoms' movement, and separates this into
+molecular vibrations - like a ball now rattling around near the bottom of a bowl. A molecule's vibrations can be
+measured experimentally by seeing which light frequencies are best absorbed by the molecule, and they're also
+important for simulating the molecule later on!
+''')
+
+
+@solara.component
+def Step3():
+    with solara.Details(summary=solara.Markdown('### Step 3: Calculating molecular vibrations'), expand=False):
+        with solara.lab.Tabs():
+            with solara.lab.Tab('Instructions: Click the buttons!'):
+                solara.Markdown(
+'''
+<ol>
+    <li><i>This next step may take <b>up to 20 minutes</b> for molecules with <b>more than twelve atoms</b></i>:
+        <ul><li>click the "Find Molecular Vibrations" button below.</li></ul>
+    </li>
+    <li>This will start the quantum calculations. While you wait for results, you can do the next section ("Measure angles and distances in Molview").</li>
+    <li>
+    Now click the "Show Vibrations" button. You will see a list of vibration frequencies. Selecting a frequency will show which atoms in the molecule vibrate at that frequency.
+    <ul>
+        <li>Is there a pattern to which atoms vibrate at lower frequencies (smaller numbers), and which atoms vibrate at higher frequencies?</li>
+        <li>The "THz" unit is 1 _terahertz_, or a trillion vibrations per second!</li>
+    </ul>
+    </li>
+</ol>
+''')
+            with solara.lab.Tab('Information: Vibrations and Spectroscopy'):
+                solara.Markdown(
+'''
+In gases, molecules only vibrate at certain frequencies (as your quantum calculation shows).
+This means they also only absorb light at certain frequencies, or wavelengths.
+The frequencies you have calculated will fall between 1 and 400 THz, which falls under _infrared radiation_.
+Infrared radiation can't be seen by human eyes, but we can feel it as heat (such as from a barbecue grill!).
+
+The Earth's atmosphere stays warm (and is getting warmer) because gases in the atmosphere absorb infrared radiation.
+By measuring which wavelengths different molecules absorb, scientists can also get information about how long bonds
+are in molecules, giving more clues about their structure. Finally, knowing how molecules vibrate is important for
+simulating them - like you're going to do later today!
+''')
+
+
+@solara.component
+def Step4():
+    with solara.Details(summary=solara.Markdown('### Step 4: Measure angles and distances in Molview'), expand=False):
+        with solara.lab.Tabs():
+            with solara.lab.Tab('Instructions: Molview Measurements'):
+                solara.Markdown(
+'''
+<ol>
+    <li>If you'd like to simulate your own molecule, you can get a SMILES code from Molview using the process in Step 1.</li>
+
+    <li>
+    While you're waiting for the quantum calculations to run, you can also use Molview to measure approximate bond lengths and angles:
+    <ol>
+        <li>Look for the "2D to 3D" button on the top bar. Click it to create an approximate 3D representation of your molecule!</li>
+        <li>Now look for the "Jmol" menu on the top bar. When you open the menu, you will see three options at the bottom under "Measurement": Distance, Angle and Torsion.</li>
+        <li>Measure different bond lengths and angles in your molecule (see the "Bond Length" and "Angle" tabs for detailed instructions). Are each of the bond lengths the same, or different? Are each of the angles the same, or different? How do the measurements compare to your quantum results? Try to make sense of what you see.</li>
+    </ol
+    </li>
+
+    <li>
+    You can also look at the polarity of your molecule!
+    <ul>
+        <li>In the "Jmol" menu, click on "MEP Surface Lucent". This will show a surface around your molecule representing the electric potential energy (using Coulomb's law!). Bluer areas are more positive, while redder areas are more negative.</li>
+    </ul>
+    </li>
+</ol>
+''')
+            with solara.lab.Tab('Bond Lengths'):
+                solara.Markdown(
+'''
+1. In the "Jmol" menu, click the "Distance" option.
+2. Now click on one of the atoms in the _3D_ view of the molecule. When you move the mouse away, you should now see a dotted line following your mouse position.
+3. Click on another atom. Now Molview will show the distance between the two chosen atoms (which is the bond length, if those atoms share a bond). The letters "nm" stand for _nanometer_, or one billionth of a meter.
+4. To remove all the numbers being shown, use the "Clear" button in the "Jmol" menu.
+''')
+            with solara.lab.Tab('Bond Angles'):
+                solara.Markdown(
+'''
+1. In the "Jmol" menu, click the "Angle" option.
+2. Now click on one of the atoms in the _3D_ view of the molecule. When you move the mouse away, you should now see a dotted line following your mouse position.
+3. Click on another two atoms. Now Molview will show the angle formed between the three chosen atoms, in degrees.
+4. To remove all the numbers being shown, click again on the "2D to 3D" button you used earlier.
+''')
+            with solara.lab.Tab('Information: Bonds, Angles and Tortions'):
+                solara.Markdown(
+'''
+A molecule's geometry can be described in terms of how long bonds are between atoms, and what angles
+those bonds make to each other. (A _torsion_ is the angle formed between _four_ atoms, which changes
+when one pair twists against the second pair.) This lets computer programs predict how molecules will
+move by treating them as little balls and sticks!
+
+The atoms between bonds are too small to see directly -- so how do scientists know what shapes molecules have?
+One of the most important ways is through _X-ray diffraction_. In a solid crystal, molecules are arranged in
+a regular order, and strong X-ray beams can reveal the spacings between different atoms.
+
+A famous example of this was when Rosalind Franklin produced the first X-ray diffraction pictures of DNA
+crystals in the 1950s. Her data was necessary for James Watson, Francis Crick and Maurice Wilkins to piece
+together the exact arrangement of atoms in the DNA double helix, for which they won the Nobel Prize in 1962.
+''')
